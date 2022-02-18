e286b8c6bc4b8507ab140e19aa4cbb2aac269858
boitan <yongrou74@hotmail.com>
2022-02-18 13:05:39 +0800

    org chart

--- app/Views/admin/car/area_list.php
+++
@@ -1,3 +0,0 @@
-<?php foreach($area as $row){ ?>
-    <option value="<?= $row['area_id'] ?>" <?= $row['area_id'] == $selected_area ? 'selected' : '' ?>><?= $row['name'] ?></option>
-<?php } ?>
--- app/Views/admin/car/model_list.php
+++
@@ -1,3 +0,0 @@
-<?php foreach($model as $row){ ?>
-    <option value="<?= $row['model_id'] ?>" <?= $row['model_id'] == $selected_model ? 'selected' : '' ?>><?= $row['name'] ?></option>
-<?php } ?>
