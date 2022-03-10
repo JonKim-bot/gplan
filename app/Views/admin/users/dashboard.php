2cf0eff6c6dd25c9ffad50bf7b1cf82e2f89f7dc
Jon Kim <yongrou74@hotmail.com>
2022-03-11 02:15:18 +0800

    save

--- app/Views/admin/users/dashboard.php
+++ app/Views/admin/users/dashboard.php
@@ -43,7 +43,6 @@
 </div> -->
 
 </header>
-
 <div class="c-body">
   <main class="c-main">
     <div class="container-fluid">
@@ -75,6 +74,7 @@
                     </p>
                   </div>
                 </div>
+
               </div>
               <!-- <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;"> -->
               <!-- <canvas class="chart" id="card-chart1" height="70"></canvas> -->
@@ -93,7 +93,7 @@
                     <p class="m-0" style="font-size: 12px; line-height:1; padding:0 5px">
                       Status
                     </p>
-                                </a>
+                      </a>
 
                 </div>
               </div>
@@ -109,13 +109,16 @@
               </div>
               <div class="col-3 d-flex justify-content-center">
                 <div class="c-Circle text-white rounded-circle d-flex flex-column justify-content-center align-items-center " style="background:linear-gradient(to right top, #2e8dab, #1980a7, #0972a2, #0e649b, #1d5592, #22528f, #274f8d, #2b4c8a, #29538e, #295b92, #2b6295, #2f6998)!important;">
-                  <div class="icon mb-2">
+                <a class="card-header-action" class="btn btn-primary" data-toggle="modal" data-target="#referal_modal">
+
+                <div class="icon mb-2">
                     <i class="fa fa-user fa-lg"></i>
                   </div>
                   <p class="m-0" style="font-size: 12px; line-height:1; padding:0 5px">
                     Referral
                   </p>
                 </div>
+</a>
               </div>
               <div class="col-3 d-flex justify-content-center">
                 <div class="c-Circle text-white rounded-circle d-flex flex-column justify-content-center align-items-center " style="background:linear-gradient(to right top, #2e8dab, #1980a7, #0972a2, #0e649b, #1d5592, #22528f, #274f8d, #2b4c8a, #29538e, #295b92, #2b6295, #2f6998)!important;">
@@ -161,7 +164,6 @@
         <!-- /.row-->
       </div>
       
-
 
       <div class="modal fade" id="status_modal" tabindex="-1" role="dialog" aria-labelledby="modalAddLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -211,4 +213,91 @@
             </div>
         </div>
     </div>
+
+
+
+    <div class="modal fade" id="referal_modal" tabindex="-1" role="dialog" aria-labelledby="modalAddLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalAddLabel">Referal</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form enctype="multipart/form-data"  method="POST" action="<?=base_url();?>">
+                <div class="modal-body">
+       
+                <div class="card text-white bg-gradient-primary">
+                    <div class="card-status card-body card-body pb-0 d-flex justify-content-between align-items-start">
+                      <div>
+                      <i class="fa fa-compass fa-2x" ></i>
+
+                        <div>Status</div>
+                        <div class="text-value-lg">
+                        <?= $users['is_verified'] == 1
+                        ? 'Verified'
+                        : 'Not verified' ?>
+                        </div>
+                      </div>
+                      <div class="btn-group">
+                        <!-- <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <svg class="c-icon">
+                            <use xlink:href="node_modules/@coreui/icons/sprites/free.svg#cil-settings"></use>
+                          </svg>
+                        </button> -->
+                        <!-- <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><a class="dropdown-item" href="#">Something else here</a></div> -->
+                      </div>
+                    </div>
+                    <!-- <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;"> -->
+                      <!-- <canvas class="chart" id="card-chart1" height="70"></canvas> -->
+                    <!-- </div> -->
+                  </div>
+                    <input type="hidden" name="asd" value="asd">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+
+
+<!--     
+
+      <div class="modal fade" id="status_modal" tabindex="-1" role="dialog" aria-labelledby="modalAddLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalAddLabel">Add Detail</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form enctype="multipart/form-data"  method="POST" action="<?=base_url();?>">
+                <div class="modal-body">
+       
+                <div class="form-group">
+                                <label for="">Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="Name" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" class="form-control" name="password" placeholder="Password" required>
+                            </div>
+
+                    <input type="hidden" name="asd" value="asd">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div> -->
 
