dc0c318661eb0fcb235dce9ebe50aaeaac690e45
unknown <sakuralun96@hotmail.com>
2022-03-11 17:46:50 +0800

    s

--- app/Language/en_lang.php
+++ app/Language/en_lang.php
@@ -1,6 +1,28 @@
+<?php
+$lang['your_point'] = 'Your point';
+$lang['withdraw'] = 'Withdraw';
+$lang['transaction_history'] = 'Transaction History';
+$lang['total_earn_amount'] = 'Total Earn Amount';
+$lang['withdraw_amount'] = 'Withdraw Amount';
+$lang['my_group'] = 'My Group';
+$lang['status'] = 'Status';
+$lang['reward'] = 'Reward';
+$lang['your_teacher'] = 'Your Teacher';
+$lang['achievement'] = 'Achievement';
+$lang['invite_more'] = 'invite more friends to get more rewards';
+$lang['wallet_withdraw'] = 'Wallet Withdraw';
+$lang['save'] = 'Save';
+$lang['username'] = 'User Name';
+$lang['contact'] = 'Contact';
+$lang['balance'] = 'Balance';
+$lang['credit'] = 'Credit';
+$lang['debit'] = 'Debit';
+$lang['remarks'] = 'Remarks';
+$lang['created_date'] = 'Created Date';
+$lang['verified'] = 'Verified';
+$lang['not_verified'] = 'Not verified';
+$lang['dont_have_family'] = 'Dont Have Family Yet';
+$lang['view_reward'] = 'View Reward';
 
-<?php 
-    $lang['your_point'] = 'Your point';
-   
 
 ?>
